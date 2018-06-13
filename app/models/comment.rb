2bac17a0498e0aba2f2a7fe6980dd29a8ff79e34
squamuglia <maxsmouha@gmail.com>
2018-06-13 17:26:07 -0400

    Done.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,12 @@ class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
 
+  accepts_nested_attributes_for :user, reject_if: :username_blank
+
+  private
+  def username_blank(user)
+    user[:username].empty?
+  end
+
+
 end
