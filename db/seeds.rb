8c3cc55dca714cd8a5e7ce8f9a18229e7ca54925
Alberto Carreras <a.carreras.c@gmail.com>
2018-06-14 06:36:11 -0400

    Solved lab

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+u1 = User.create(username: "User1" , email: "u1@gmail.com")
+u2 = User.create(username: "User2" , email: "u2@gmail.com")
+
+p1 = Post.create(title: "Test" , content: "Test content")
+p2 = Post.create(title: "Test2" , content: "Test2 content")
+
+c1 = Category.create(name: "Sex")
+c2 = Category.create(name: "Drugs")
+
+pc1 = PostCategory.create(post_id: p1[:id] , category_id: c2[:id])
+pc1 = PostCategory.create(post_id: p2[:id] , category_id: c1[:id])
+
+com1 = Comment.create(post_id: p1[:id] , user_id: u2[:id], content: "Com Content 1")
+com2 = Comment.create(post_id: p2[:id] , user_id: u1[:id], content: "Com Content 1")
